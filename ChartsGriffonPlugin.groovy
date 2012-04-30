8da6be7f36f0f12918cad5213d75d32cd2538530
aalmiray <aalmiray@gmail.com>
2012-04-30 19:02:54 +0200

    Fix plugin description (take 2)

--- ChartsGriffonPlugin.groovy
+++ ChartsGriffonPlugin.groovy
@@ -38,7 +38,7 @@ class ChartsGriffonPlugin {
     // URL where documentation can be found
     String documentation = ''
     // URL where source can be found
-    String source = 'https://github.com/griffon/griffon-colorchooser-plugin'
+    String source = 'https://github.com/griffon/griffon-charts-plugin'
 
     List authors = [
         [
@@ -56,78 +56,78 @@ Usage
 
 The following nodes will become available on a View script upon installing this plugin
 
-| Node  | Property          | Type                | Default                                | Required | Bindable |
-| ----- | ----------------- | ------------------- | -------------------------------------- | -------- | -------- |
-| chart | chart             | JFreeChart or Class |                                        | yes      | no       |
-|       | width             | int                 | ChartPanel.DEFAULT_WIDTH               | no       | no       |
-|       | height            | int                 | ChartPanel.DEFAULT_HEIGHT              | no       | no       |
-|       | minimumDrawWidth  | int                 | ChartPanel.DEFAULT_MINIMUM_DRAW_WIDTH  | no       | no       |
-|       | minimumDrawHeight | int                 | ChartPanel.DEFAULT_MINIMUM_DRAW_HEIGHT | no       | no       |
-|       | maximumDrawWidth  | int                 | ChartPanel.DEFAULT_MAXIMUM_DRAW_WIDTH  | no       | no       |
-|       | maximumDrawHeight | int                 | ChartPanel.DEFAULT_MAXIMUM_DRAW_HEIGHT | no       | no       |
-|       | useBuffer         | boolean             | true                                   | no       | no       |
-|       | properties        | boolean             | true                                   | no       | no       |
-|       | copy              | boolean             | true                                   | no       | no       |
-|       | save              | boolean             | true                                   | no       | no       |
-|       | print             | boolean             | true                                   | no       | no       |
-|       | zoom              | boolean             | true                                   | no       | no       |
-|       | tooltips          | boolean             | true                                   | no       | no       |
+| *Node* | *Property*        | *Type*              |
+| ------ | ----------------- | ------------------- |
+| chart  | chart             | JFreeChart or Class |
+|        | width             | int                 |
+|        | height            | int                 |
+|        | minimumDrawWidth  | int                 |
+|        | minimumDrawHeight | int                 |
+|        | maximumDrawWidth  | int                 |
+|        | maximumDrawHeight | int                 |
+|        | useBuffer         | boolean             |
+|        | properties        | boolean             |
+|        | copy              | boolean             |
+|        | save              | boolean             |
+|        | print             | boolean             |
+|        | zoom              | boolean             |
+|        | tooltips          | boolean             |
 
-If a Class is set as the value for `chart:` then it's assumed to be a script that can be parsed using ChartBuilder 
+If a Class is set as the value for `chart:` then it is assumed to be a script that can be parsed using ChartBuilder
 
 ### Scripts
 
- * **create-chart** - creates a new Chart script that can be parsed using ChartBuilder
+ * __create-chart__ - creates a new Chart script that can be parsed using ChartBuilder
 
 ### Example
 
-Chart scripts have access to all variables form their surrounding contextt. This means that a chart can read properties
-from a Model if the chart is built inside a View script. Here's how the typical chart Script looks like
+Chart scripts have access to all variables form their surrounding context. This means that a chart can read properties
+from a Model if the chart is built inside a View script. Here is how the typical chart Script looks like
 
 __griffon-app/conf/charts/sample/SampleChart.groovy__
-        
-        package sample
+
+		package sample
 		import java.awt.Color
 		import java.awt.Font
 		import org.jfree.chart.labels.PieToolTipGenerator
- 
-		def largeFont = new Font("Arial", Font.BOLD, 15);
- 
-		piechart3d(title: "Simple Pie Chart") {
-		    defaultPieDataset {
-		        Series1(40.0f)
-		        Series2(30.0f)
-		        Series3(30.0f)
-		    }
-		    antiAlias = true
-		    backgroundPaint(Color.WHITE)
- 
-		    pieplot {
-		        sectionOutlinesVisible false
-		        labelFont largeFont
-		        labelGap 0.02
-		        toolTipGenerator ({ dataset, key -> return "[${dataset} ${key}]" as String } as PieToolTipGenerator)
- 
-		        sectionPaint('Series1', paint: new Color(255,0,0))
-		        sectionPaint('Series2', paint: new Color(0,255,0))
-		        sectionPaint('Series3', paint: new Color(0,0,255))
-		    }
+
+		def largeFont = new Font('Arial', Font.BOLD, 15)
+
+		piechart3d(title: 'Simple Pie Chart') {
+			defaultPieDataset {
+				Series1(40.0f)
+				Series2(30.0f)
+				Series3(30.0f)
+			}
+			antiAlias = true
+			backgroundPaint(Color.WHITE)
+
+			pieplot {
+				sectionOutlinesVisible false
+				labelFont largeFont
+				labelGap 0.02
+				toolTipGenerator ({ dataset, key -> return "[${dataset} ${key}]" as String } as PieToolTipGenerator)
+
+				sectionPaint('Series1', paint: new Color(255,0,0))
+				sectionPaint('Series2', paint: new Color(0,255,0))
+				sectionPaint('Series3', paint: new Color(0,0,255))
+			}
 		}
 
 This is how the previously defined chart may be embedded in a View
 
 __griffon-app/views/sample/SampleView.groovy__
 
-        package sample
+		package sample
 		application(title: 'Charts',
-		  preferredSize: [320, 240],
-		  pack: true,
-		  locationByPlatform:true,
-		  iconImage: imageIcon('/griffon-icon-48x48.png').image,
-		  iconImages: [imageIcon('/griffon-icon-48x48.png').image,
-		               imageIcon('/griffon-icon-32x32.png').image,
-		               imageIcon('/griffon-icon-16x16.png').image]) {
-		    chart(SampleChart)
+				preferredSize: [320, 240],
+				pack: true,
+				locationByPlatform:true,
+				iconImage: imageIcon('/griffon-icon-48x48.png').image,
+				iconImages: [imageIcon('/griffon-icon-48x48.png').image,
+				imageIcon('/griffon-icon-32x32.png').image,
+				imageIcon('/griffon-icon-16x16.png').image]) {
+			chart(SampleChart)
 		}
 
 [1]: http://www.jfree.org/jfreechart
